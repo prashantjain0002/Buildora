1528f8b41a00ee90b3d8a9f301a11a413a84ae46
Prashant Jain <prashantjain0002@gmail.com>
2025-07-03 12:37:08 +0530

    setup inngest

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
-import { caller } from "@/trpc/server";
+"use client";
 
-const page = async () => {
-  const data = await caller.createAI({ text: "hello" });
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
 
-  return <div>{JSON.stringify(data)}</div>;
+const page =  () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+
+  return (
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button onClick={() => invoke.mutate({ text: "Prashant" })}>
+        Invoke the Job
+      </Button>
+    </div>
+  );
 };
 
 export default page;
